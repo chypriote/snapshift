cc682113943c5a4f33fd37c52d881f3fb1b754b2
ChypRiotE <nicolastem@hotmail.fr>
2016-08-28 22:25:03 +0200

    animations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
+var smController = new ScrollMagic.Controller();
+
+/* Slide In Right Animation */
+var transXAnimation = new TimelineMax()
+	.to(".img-planning", 2, {x: "-75%"})
+new ScrollMagic.Scene({
+		duration: 700,
+		triggerElement: '#hero .sub-title',
+		triggerHook: "onLeave"
+	})
+	.setTween(transXAnimation)
+	.addTo(smController)
+
+/* Pop In Animation */
+var popinAnimation = new TimelineMax()
+	.to(".img-contract", 2, {scale: 1})
+
+
+new ScrollMagic.Scene({
+		duration: 600,
+		triggerElement: '#presences',
+		triggerHook: "onLeave"
+	})
+	.setTween(popinAnimation)
+	.addTo(smController)
+
 $.fn.extend({
 	animateCss: function (animationName) {
 		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
@@ -23,27 +49,27 @@ $(window).scroll(function () {
 	}
 });
 
-var planning = new Waypoint({
-	element: document.getElementById('users'),
-	handler: function(direction) {
-		if (direction == 'down') {
-			$('.img-planning').animateCss('slideInRight');
-			$('.img-contract').removeClass('notVisible');
-		} else {
-				$('.img-planning').disanimateCss('slideOutRight');
-		}
-	},
-	offset: 400
-});
+// var planning = new Waypoint({
+// 	element: document.getElementById('users'),
+// 	handler: function(direction) {
+// 		if (direction == 'down') {
+// 			$('.img-planning').animateCss('slideInRight');
+// 			$('.img-contract').removeClass('notVisible');
+// 		} else {
+// 				$('.img-planning').disanimateCss('slideOutRight');
+// 		}
+// 	},
+// 	offset: 400
+// });
 
-var contract = new Waypoint({
-	element: document.getElementById('presences'),
-	handler: function(direction) {
-		if (direction == 'down') {
-			$('.img-contract').animateCss('zoomIn');
-			$('.img-contract').removeClass('notVisible');
-		} else {
-			$('.img-contract').disanimateCss('zoomOut');
-		}
-	}
-});
+// var contract = new Waypoint({
+// 	element: document.getElementById('presences'),
+// 	handler: function(direction) {
+// 		if (direction == 'down') {
+// 			$('.img-contract').animateCss('zoomIn');
+// 			$('.img-contract').removeClass('notVisible');
+// 		} else {
+// 			$('.img-contract').disanimateCss('zoomOut');
+// 		}
+// 	}
+// });
